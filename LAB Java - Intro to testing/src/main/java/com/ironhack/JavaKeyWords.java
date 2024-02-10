d79f93e1c0e5243ffdcb3eb2f2fe5aa643760d5e
Estefania Amundaray <estefania.amundaray@emburse.com>
2024-02-10 15:40:22 +0100

    exercise 2 tess no pass

---
+++ LAB Java - Intro to testing/src/main/java/com/ironhack/JavaKeyWords.java
@@ -0,0 +1,7 @@
+package com.ironhack;
+
+public class JavaKeyWords {
+    public static boolean javaKeyWords(String input_string){
+        return true;
+    }
+}
---
+++ LAB Java - Intro to testing/src/test/java/com/ironhack/JavaKeyWordsTest.java
@@ -0,0 +1,60 @@
+package com.ironhack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JavaKeyWordsTest {
+    @Test
+    @DisplayName("Test basic Functionality contains One Keyword")
+    public void testContainsOneKeyword(){
+
+    }
+
+    @Test
+    @DisplayName("Test basic Functionality contains Multiple Keyword")
+    public void testContainsMultipleKeyword(){
+
+    }
+
+    @Test
+    @DisplayName("Test basic Functionality contains NO Keyword")
+    public void testContainsNoKeyword(){
+
+    }
+    @Test
+    @DisplayName("Test empty String")
+    public void testEmptyString(){
+
+    }
+    @Test
+    @DisplayName("Test Incomplete Keyword break bre")
+    public void testIncompleteKeyword(){
+
+    }
+    @Test
+    @DisplayName("Test is a Keyword break break")
+    public void testIsAKeyword(){
+
+    }
+
+    @Test
+    @DisplayName("Test Complete Keyword plus additional chars break breaker")
+    public void testCompleteKeywordAdditionalChars(){
+
+    }
+    @Test
+    @DisplayName("Test Case Sensitive")
+    public void testCaseInsensitive(){
+
+    }
+    @Test
+    @DisplayName("Test Special Chars")
+    public void testContainsSpecialChars(){
+
+    }
+    @Test
+    @DisplayName("Test Null Input")
+    public void testNullInput(){
+
+    }
+}
